b5482ada7b37ffd8f939d686898787bf31a15974
Rohith09 <kandirohith09@gmail.com>
2018-03-01 21:43:59 +0530

    Minor changes and Added Files.

---
+++ app/src/main/java/com/example/chandrassekhar/newsreaderapp/Model/Source.java
@@ -0,0 +1,23 @@
+package com.example.chandrassekhar.newsreaderapp.Model;
+
+
+import java.util.List;
+class UrlsToLogos{
+    private String small,medium,large;
+
+}
+
+public class Source {
+    private String id;
+    private String name;
+    private String description;
+    private String url;
+    private String category;
+    private String language;
+    private String country;
+    private UrlsToLogos urlsToLogos;
+    private List<String> sortBysAvailable;
+
+
+
+}
