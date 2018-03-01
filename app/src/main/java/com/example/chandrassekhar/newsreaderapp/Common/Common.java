7e61dea59c1f74af614e1464918034f787882b0e
Rohith09 <kandirohith09@gmail.com>
2018-03-01 22:05:48 +0530

    Minor changes

--- app/src/main/java/com/example/chandrassekhar/newsreaderapp/Common/Common.java
+++ app/src/main/java/com/example/chandrassekhar/newsreaderapp/Common/Common.java
@@ -1,8 +1,17 @@
 package com.example.chandrassekhar.newsreaderapp.Common;
 
-/**
- * Created by chandrassekhar on 01/03/18.
- */
+
+import com.example.chandrassekhar.newsreaderapp.Interface.NewsService;
+import com.example.chandrassekhar.newsreaderapp.Remote.RetrofitClient;
 
 public class Common {
+    private static final String BASE_URL="https://newsapi.org/";
+
+    public static final String API_KEY="61c7c0c3f69e4fbb8da7082fa1226ff4";
+
+    public static NewsService getNewsService()
+    {
+        return RetrofitClient.getClient(BASE_URL).create(NewsService.class);
+    }
 }
+
